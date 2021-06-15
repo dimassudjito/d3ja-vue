ee69ea719dfa10ccb1cfea2aa8674ebd31148e5c
Dimas Sudjito <almuhtad@ualberta.ca>
2021-06-14 20:29:27 -0600

    initialize library with the first component

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <TreeOfLife />
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import { TreeOfLife } from '@/entry.esm'
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    TreeOfLife
+  }
+})
+</script>
---
+++ src/lib-components/index.js
@@ -0,0 +1,2 @@
+/* eslint-disable import/prefer-default-export */
+export { default as TreeOfLife } from './TreeOfLife.vue'
